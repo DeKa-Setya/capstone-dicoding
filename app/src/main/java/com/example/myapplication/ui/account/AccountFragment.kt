cdc985240af41afddefd164138bf9a89afd06618
seablue25 <arianastama@gmail.com>
2023-05-28 11:54:25 +0700

    acc frags

--- app/src/main/java/com/example/myapplication/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/account/AccountFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentAccountBinding
+import de.hdodenhof.circleimageview.CircleImageView
 
 class AccountFragment : Fragment() {
 
@@ -22,16 +25,19 @@ class AccountFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val accountViewModel =
-            ViewModelProvider(this).get(AccountViewModel::class.java)
+//        val accountViewModel =
+//            ViewModelProvider(this).get(AccountViewModel::class.java)
 
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        accountViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textNotifications
+//        accountViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        val circleImageView: CircleImageView = binding.circleImageView
+        circleImageView.borderWidth = 2
+        circleImageView.borderColor = activity?.let { ContextCompat.getColor(it, R.color.black) }!!
         return root
     }
 
